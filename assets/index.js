ecb8444f943bc9de895aee1d19a1a87ea10878bf
Pedro Freire <pedro-of@live.com>
2019-10-16 21:03:44 -0300

    removing jquery and omitization

--- assets/index.js
+++ assets/index.js
@@ -1,78 +1,240 @@
+if ( !Array.prototype.forEach ) {
+    Array.prototype.forEach = function(fn, scope) {
+      for(var i = 0, len = this.length; i < len; ++i) {
+        fn.call(scope, this[i], i, this);
+      }
+    };
+}
+
+function fadeIn(el, display){
+    if(!el){
+        return;
+    }
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .1) > 1)) {
+        el.style.opacity = val;
+        requestAnimationFrame(fade);
+        }
+    })();
+};
+
+function fadeOut(el){
+    if(!el){
+        return;
+    }
+    el.style.opacity = 1;
+
+    (function fade() {
+        if ((el.style.opacity -= .1) < 0) {
+        el.style.display = "none";
+        } else {
+        requestAnimationFrame(fade);
+        }
+    })();
+};
+
+
 var offheight = 100;
-var nav = $('.navigation-collapse');
-var animateIcon = $('.animated-icon');
+var nav = document.querySelector('.navigation-collapse');
+var animateIcon = document.querySelector('.animated-icon');
+var navigation = document.querySelector('.navigation');
 
-$('.nav-button').on('click', function () {
-    animateIcon.toggleClass('open');
-    nav.toggleClass('open');
+
+document.querySelector('.nav-button').addEventListener('click', function () {
+    animateIcon.classList.toggle('open');
+    nav.classList.toggle('open');
 });
-
-nav.on("click", "a", function () {
-    animateIcon.removeClass('open');
-    nav.removeClass('open');
+document.querySelectorAll('a').forEach(function (element){
+    element.addEventListener("click", function () {
+        animateIcon.classList.remove('open');
+        nav.classList.remove('open');
+    });
 });
 
 // smooth scroll js
-$(window).scroll(function () {
-    var scrolling = $(this).scrollTop();
-    var sticky = $('.navigation');
+window.addEventListener("scroll", function(element){
+    var scrolling = window.screenY;
     if (scrolling >= 100) {
-        $(sticky).addClass("nav-bg");
+       navigation.classList.add("nav-bg");
     } else {
-        $(sticky).removeClass("nav-bg");
+        navigation.classList.remove("nav-bg");
     }
-    if (scrolling >= 50) {
-        $('.backtotop').fadeIn(500);
-    } else {
-        $('.backtotop').fadeOut(500);
-    }
-
+    // if (scrolling >= 50) {
+    //     $('.backtotop').fadeIn(500);
+    // } else {
+    //     $('.backtotop').fadeOut(500);
+    // }
     calculateNavActive();
 });
 
 
 var calculateNavActive = function(e){
-  var scroll = $(window).scrollTop();
+  var scroll = window.screenY;
 
-  $('.nav-link.active').removeClass('active');
+  try{
+    document.querySelector('.nav-link.active').classList.remove('active');
+  }catch(e){
+
+  }
 
   var lastId;
-  $('[data-section-name]').each(function(_, chapter) {
-      var id = chapter.id;
-
-      if($(chapter).offset().top-offheight < scroll) {
-         lastId = id;
-      }
-  });
-     $('.nav-link[href=\'#' + lastId + '\']').addClass('active');
-}
-
-$(document).on('click', 'a[href^="#"]',function(){
-    var objeto = $($(this).attr('href'));
-    if(objeto.length > 0)
-        $('html,body').animate({scrollTop: objeto.offset().top},'slow');
-});
-
-$(document).on('click', '.alert-button',function(){
-    $(".alert-container").fadeOut(500);
-
-    $('.valid').removeClass('valid');
-});
-
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.scrollup').fadeIn();
-        } else {
-            $('.scrollup').fadeOut();
+  document.querySelectorAll('[data-section-name]').forEach(function(chapter){
+    var id = chapter.id;
+        if(chapter.offsetTop-offheight < scroll) {
+            lastId = id;
         }
     });
 
-    $('.scrollup').click(function () {
-        $("html, body").animate({
-            scrollTop: 0
-        }, 600);
+    if(lastId){
+        document.querySelector('.nav-link[href=\'#' + lastId + '\']').classList.add('active');
+    }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(function (element){
+    element.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        var href = element.getAttribute("href");
+        if(href.length == 1){
+            return false;
+        }
+        var objeto = document.querySelector(href);
+
+        if(objeto){
+           scrollTo(objeto.offsetTop);
+        }
         return false;
     });
-    calculateNavActive();
 });
+
+function scrollTo(element) {
+    window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: element,
+      });
+}
+
+document.querySelector('.alert-button').addEventListener('click', function(){
+    fadeOut(document.querySelector('.alert-container'));
+    // classList.remove('valid');
+});
+
+(function(){
+    window.addEventListener("scroll", function(){
+        if (window.scrollY > 100) {
+            fadeIn(document.querySelector('.scrollup'));
+        } else {
+            fadeOut(document.querySelector('.scrollup'));
+        }
+    });
+
+    document.querySelector('.scrollup').addEventListener("click", function(){
+        window.scroll({
+            behavior: 'smooth',
+            left: 0,
+            top: 0,
+          });
+    });
+    calculateNavActive();
+
+
+
+    window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('gform');
+    // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            if (form.checkValidity() ) {
+                var data = getFormData(form);
+                var url = form.action;
+                var xhr = new XMLHttpRequest();
+                disableAllButtons(form);
+                xhr.open('POST', url);
+                // xhr.withCredentials = true;
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.onreadystatechange = function() {
+                    form.reset();
+                    var formElements = form.querySelector(".form-elements")
+
+                    var thankYouMessage = document.querySelector(".alert-container");
+                    if (thankYouMessage) {
+                        thankYouMessage.style.display = "flex";
+                    }
+                    form.classList.remove('was-validated');
+                    return;
+                };
+                // url encode form data for sending as post data
+                var encoded = Object.keys(data).map(function(k) {
+                    return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+                }).join('&');
+                xhr.send(encoded);
+            }
+            form.classList.add('was-validated');
+        }, false);
+        });
+    }, false);
+
+
+    // get all data in form and return object
+    function getFormData(form) {
+        var elements = form.elements;
+
+        var fields = Object.keys(elements).filter(function(k) {
+            return (elements[k].name !== "honeypot");
+        }).map(function(k) {
+        if(elements[k].name !== undefined) {
+            return elements[k].name;
+        // special case for Edge's html collection
+        }else if(elements[k].length > 0){
+            return elements[k].item(0).name;
+        }
+        }).filter(function(item, pos, self) {
+        return self.indexOf(item) == pos && item;
+        });
+
+        var formData = {};
+        fields.forEach(function(name){
+        var element = elements[name];
+
+        // singular form elements just have one value
+        formData[name] = element.value;
+
+        // when our element has multiple items, get their values
+        if (element.length) {
+            var data = [];
+            for (var i = 0; i < element.length; i++) {
+            var item = element.item(i);
+            if (item.checked || item.selected) {
+                data.push(item.value);
+            }
+            }
+            formData[name] = data.join(', ');
+        }
+        });
+
+        // add form-specific values into the data
+        formData.formDataNameOrder = JSON.stringify(fields);
+        formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+        formData.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+
+        console.log(formData);
+        return formData;
+    }
+
+
+    function disableAllButtons(form) {
+        var buttons = form.querySelectorAll("button");
+        for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+        }
+    }
+
+})();
