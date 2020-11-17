053a7e0f4fb196b8c6de930bf0a08111a92280f1
pofreire <30freire@gmail.com>
2020-11-17 01:25:37 +0000

    Deploying to gh-pages from master @ 1ba3dba6ba6a77a6ee9fc812ef755f739090ab90 🚀

---
+++ precache-manifest.0761706329fda225a363b10e784ba94f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a710377392482d50ca6df1d63716984",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbb3ab9822c779da6aef",
+    "url": "/static/css/main.cc504e42.chunk.css"
+  },
+  {
+    "revision": "8c408c2bac063cd17ce9",
+    "url": "/static/js/2.01a85759.chunk.js"
+  },
+  {
+    "revision": "f9689ab3bdd81059c30c22857445580d",
+    "url": "/static/js/2.01a85759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb3ab9822c779da6aef",
+    "url": "/static/js/main.8a653aeb.chunk.js"
+  },
+  {
+    "revision": "61d3044d55b1ab6ab842",
+    "url": "/static/js/runtime-main.f01603a9.js"
+  },
+  {
+    "revision": "99553948fb119d974c09e9f7ec1850e9",
+    "url": "/static/media/caci.99553948.png"
+  },
+  {
+    "revision": "44f3a5a1476e02dc098e0059a19a265f",
+    "url": "/static/media/cognicao-e-linguagem.44f3a5a1.svg"
+  },
+  {
+    "revision": "33de0c2a3a73b968b6b9822c45ad0596",
+    "url": "/static/media/ded.33de0c2a.svg"
+  },
+  {
+    "revision": "18f3d6b404ff0d0f0943d2bbec752e2b",
+    "url": "/static/media/pedro.18f3d6b4.jpg"
+  },
+  {
+    "revision": "a5fae1afd0b8065c9f8cf7b086996ff6",
+    "url": "/static/media/redentor.a5fae1af.svg"
+  },
+  {
+    "revision": "e2966697cb35c793c5d27cecf9562c2b",
+    "url": "/static/media/redplus.e2966697.png"
+  },
+  {
+    "revision": "b5b7b1049448fcb887042f97da627246",
+    "url": "/static/media/reeducando.b5b7b104.png"
+  }
+]);
